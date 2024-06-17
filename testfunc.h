a1a2c63013c077a1fb5b8d0917ceb6f59bea6276
user2022101409 <2022101409@pnu.edu.ru>
2024-06-17 16:34:45 +1000

    added test function

---
+++ testfunc.h
@@ -0,0 +1,15 @@
+//testfunc
+
+#ifndef TESTFUNC_H
+#define TESTFUNC_H
+
+class Testfunc{
+public:
+    static int add(const int& a, const int& b);
+    static int subtract(const int& a, const int& b);
+    static int multiply(const int& a, const int& b);
+    static int divide(const int& a, const std::int64_t& b);
+};
+
+
+#endif // testfunc
